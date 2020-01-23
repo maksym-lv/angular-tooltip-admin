d928b6fd0c21b1e0852f2c06cc526fa6e7bbefd9
Maksym <lokhmanov.maksym@gmail.com>
2020-01-23 19:16:50 +0200

    -- (add) json-server -- (edit) image service

--- src/app/models/image.model.ts
+++ src/app/models/image.model.ts
@@ -3,13 +3,11 @@ import { TooltipConfigModel } from './tooltip-config.model';
 
 export class ImageModel {
   public id: string;
-  public category?: string;
   public url: string | ArrayBuffer;
   public tooltip_config: TooltipConfigModel;
 
   constructor(data) {
     this.id = data.id || ImageModel.getId();
-    this.category = data.category || 'view';
     this.url = data.url;
     this.tooltip_config = new TooltipConfigModel(data.tooltip_config || {});
   }
